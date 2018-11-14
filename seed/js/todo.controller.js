cf196e5690ede620a0b857f4c19f643ffb8a250f
sanem.ucan <sanem.ucan@openx.com>
2018-11-14 16:59:34 -0500

    lab4

--- seed/js/todo.controller.js
+++ seed/js/todo.controller.js
@@ -1,27 +1,34 @@
 function TodoController() {
-  this.newTodo = '';
-  this.list = [{
-    title: 'First todo Item',
-    completed: false
-  },{
-    title: 'Second todo Item',
-    completed: true
-  },{
-    title: 'Third todo Item',
-    completed: false
-  }];
-  this.addTodo = function () {
+  this.newTodo = "";
+  this.list = [
+    {
+      title: "Wash Dishes",
+      completed: false
+    },
+    {
+      title: "Go to Central Park",
+      completed: true
+    },
+    {
+      title: "Pee",
+      completed: false
+    }
+  ];
+  this.addTodo = function() {
     this.list.unshift({
       title: this.newTodo,
       completed: false
     });
-    this.newTodo = '';
+    this.newTodo = "";
   };
-  this.removeTodo = function(item, index){
-    this.list.splice(index,1);
-  }
+  this.removeTodo = function(item, index) {
+    this.list.splice(index, 1);
+  };
+  this.getRemaining = function() {
+    return this.list.filter(function(item) {
+      return !item.completed;
+    });
+  };
 }
 
-angular
-  .module('app')
-  .controller('TodocController', TodoController);
+angular.module("app").controller("TodocController", TodoController);
